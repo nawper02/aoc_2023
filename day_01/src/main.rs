5bc211779f738a47726034e441d394fd88527876
kinblandford <nawper02@gmail.com>
2023-12-01 20:46:27 -0700

    added comments

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -17,27 +17,38 @@ fn main() {
 }
 
 fn part01(input: &str) -> Result<String, String> {
+    // start sum at 0
     let mut sum = 0;
+    // loop over lines in input
     for line in input.lines() {
+        // filter out all of the digits in the line
         let digits_only: Vec<char> = line.chars().filter(|c| c.is_digit(10)).collect();
+        // the number for the line is 10x the left ([0]) digit plus the right ([-1]) digit
         let num = digits_only[0].to_digit(10).unwrap() * 10 + digits_only[digits_only.len() - 1].to_digit(10).unwrap();
+        //add the number for the line to the sum
         sum += num as i32;
     }
     Ok(sum.to_string())
 }
 
 fn part02(input: &str) -> Result<String, String> {
+    // map words to number replacements. leave outer characters to prevent inner replacements
+    // from breaking outer replacements
     let num_map = vec![
         ("one", "o1e"), ("two", "t2o"), ("three", "t3e"), ("four", "f4r"),
         ("five", "f5e"), ("six", "s6x"), ("seven", "s7n"), ("eight", "e8t"),
         ("nine", "n9e"),
     ];
 
+    // turn the input into a string so we can call .replace
     let mut input_string = input.to_string();
+    // loop over the map. we take ownership here but thats fine since its not used anywhere else
     for (word, num) in num_map {
+        // replace any number words with their corresponding digit
         input_string = input_string.replace(word, num);
     }
 
+    // call part01 on the substituted text!
     part01(input_string.as_str())
 
 }
